b6d2e58779286876a5e16c4643a4e66dc421b762
NatiG <nati.g5684@gmail.com>
2022-02-22 20:06:43 +0300

    Fix linter errors

--- modules/library.js
+++ modules/library.js
@@ -2,31 +2,31 @@ const list = document.getElementById('book_list');
 const AddBook = document.getElementById('AddBook');
 
 export default class Library {
-    constructor(BooksContainer) {
-      this.books = [];
-      this.BooksContainer = BooksContainer;
-      this._storage = false;
-      this._check_storage('localStorage');
-      this._init_data();
-      this.books.forEach((book) => this.add_to_page(book));
-    }
+  constructor(BooksContainer) {
+    this.books = [];
+    this.BooksContainer = BooksContainer;
+    this._storage = false;
+    this._check_storage('localStorage');
+    this._init_data();
+    this.books.forEach((book) => this.add_to_page(book));
+  }
 
-    add_to_collection(data) {
-      const { id, title, author } = data;
-      this.books.push({
-          id,
-          title,
-          author,
-      });
+  add_to_collection(data) {
+    const { id, title, author } = data;
+    this.books.push({
+      id,
+      title,
+      author,
+    });
 
-      this._u_storage();
-      this.add_to_page(data);
-    }
+    this._u_storage();
+    this.add_to_page(data);
+  }
 
-    add_to_page(data) {
-        const { id, title, author } = data;
+  add_to_page(data) {
+    const { id, title, author } = data;
 
-        this.BooksContainer.innerHTML += `
+    this.BooksContainer.innerHTML += `
     <li id ="${id}">
     <div class="_title">
       <h3>"${title}"</h3>
@@ -38,71 +38,71 @@ export default class Library {
     </li>
     `;
 
-        this.updateEventListeners(this.BooksContainer);
-    }
+    this.updateEventListeners(this.BooksContainer);
+  }
 
-    updateEventListeners(element = document) {
-        const _rm_btn = element.querySelectorAll('.rm_book');
+  updateEventListeners(element = document) {
+    const _rm_btn = element.querySelectorAll('.rm_book');
 
-        _rm_btn.forEach((_rmBtn) => {
-            _rmBtn.addEventListener('click', (e) => {
-                const { parentNode } = e.target;
-                this.rm_book(parentNode.id);
-                parentNode.remove();
-            });
-        });
-    }
+    _rm_btn.forEach((_rmBtn) => {
+      _rmBtn.addEventListener('click', (e) => {
+        const { parentNode } = e.target;
+        this.rm_book(parentNode.id);
+        parentNode.remove();
+      });
+    });
+  }
 
-    rm_book(id) {
-        this.books = this.books.filter((book) => book.id = id === false);
-        this._u_storage();
-    }
+  rm_book(id) {
+    this.books = this.books.filter((book) => book.id = id === false);
+    this._u_storage();
+  }
 
-    _init_data() {
-        if (this._storage) {
-            const localData = window.localStorage.getItem('books');
-            if (localData) {
-                this.books = JSON.parse(localData);
-            }
-        }
+  _init_data() {
+    if (this._storage) {
+      const localData = window.localStorage.getItem('books');
+      if (localData) {
+        this.books = JSON.parse(localData);
+      }
     }
+  }
 
-    _check_storage(type) {
-        let storage;
-        try {
-            storage = window[type];
-            const st = '_test_storage_';
-            storage.setItem(st, st);
-            storage.rm_item(st);
-            this._storage = true;
-        } catch (e) {
-            this._storage = false;
-        }
+  _check_storage(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const st = '_test_storage_';
+      storage.setItem(st, st);
+      storage.rm_item(st);
+      this._storage = true;
+    } catch (e) {
+      this._storage = false;
     }
+  }
 
-    _u_storage() {
-        if (this._storage) {
-            const storage = window.localStorage;
-            storage.setItem('books', JSON.stringify(this.books));
-        }
+  _u_storage() {
+    if (this._storage) {
+      const storage = window.localStorage;
+      storage.setItem('books', JSON.stringify(this.books));
     }
+  }
 }
 
 const bookCollection = new Library(list);
 
 AddBook.addEventListener('submit', (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const id = Date.now().toString();
-    const title = AddBook.title.value.trim();
-    const author = AddBook.author.value.trim();
+  const id = Date.now().toString();
+  const title = AddBook.title.value.trim();
+  const author = AddBook.author.value.trim();
 
-    bookCollection.add_to_collection({
-        id,
-        title,
-        author,
-    });
+  bookCollection.add_to_collection({
+    id,
+    title,
+    author,
+  });
 
-    AddBook.title.value = '';
-    AddBook.author.value = '';
+  AddBook.title.value = '';
+  AddBook.author.value = '';
 });
--- modules/navigate.js
+++ modules/navigate.js
@@ -1,33 +1,33 @@
 const navigate = [
-    {
-        link: 'link_list',
-        target_ID: 'section_list',
-    },
-    {
-        link: 'add_link',
-        target_ID: 'add_section',
-    },
-    {
-        link: 'contact_link',
-        target_ID: 'contact_section',
-    },
+  {
+    link: 'link_list',
+    target_ID: 'section_list',
+  },
+  {
+    link: 'add_link',
+    target_ID: 'add_section',
+  },
+  {
+    link: 'contact_link',
+    target_ID: 'contact_section',
+  },
 ];
 
 const navigationLinks = () => {
-    navigate.forEach((navigation) => {
-        const link = document.getElementById(navigation.link);
-    
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-    
-            const sections = document.querySelectorAll('main > section');
-            sections.forEach((section) => {
-                section.classList.add('_display_f_');
-            });
-    
-            const target = document.getElementById(navigation.target_ID);
-            target.classList.remove('_display_f_');
-        });
+  navigate.forEach((navigation) => {
+    const link = document.getElementById(navigation.link);
+
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const sections = document.querySelectorAll('main > section');
+      sections.forEach((section) => {
+        section.classList.add('_display_f_');
+      });
+
+      const target = document.getElementById(navigation.target_ID);
+      target.classList.remove('_display_f_');
     });
+  });
 };
 export { navigate, navigationLinks };
