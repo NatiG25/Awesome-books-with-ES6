86f93830376a8740c8515ccabc830ebd069e8f85
NatiG <nati.g5684@gmail.com>
2022-02-22 18:23:04 +0300

    Add date variable

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import book_Collection from './modules/bookCollection.js';
 import { navigationLinks } from './modules/navigate.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 
-
-function displayDate() {
-    document.getElementById('date').innerHTML = Date();
-}
+const date = document.getElementById('date');
+date.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
 
 navigationLinks();
 new book_Collection();
